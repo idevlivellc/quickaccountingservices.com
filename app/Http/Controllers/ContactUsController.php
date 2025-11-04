c5b6cae7285138471252636a854aef494e6782f8
Sayan <sayan.das@idevlive.com>
2025-11-05 00:01:39 +0530

    Implement Telegram notification for contact form submissions; add TelegramService and update ContactUsController

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreQueryRequest;
+use App\Services\TelegramService;
 use Illuminate\Http\JsonResponse;
 
 class ContactUsController extends Controller
@@ -12,8 +13,24 @@ class ContactUsController extends Controller
      * @param StoreQueryRequest contains name, email, phone number, service and query
      * @return JsonResponse
      */
-    public function store(StoreQueryRequest $request): JsonResponse
+    public function store(StoreQueryRequest $request, TelegramService $telegram): JsonResponse
     {
-        return response()->json($request->post("query"));
+        $name = $request->post("name");
+        $email = $request->post("email");
+        $phone = $request->post("phone");
+        $service = $request->post("service");
+        $query = $request->post("query");
+
+        $text =
+        "<b>New Contact Submission</b>" . PHP_EOL . PHP_EOL .
+        "<b>Name:</b> {$name}". PHP_EOL .
+        "<b>Email:</b> {$email}". PHP_EOL .
+        "<b>Phone:</b> {$phone}". PHP_EOL .
+        "<b>Service:</b> {$service}". PHP_EOL .
+        "<b>Query:</b> {$query}";
+
+        $telegram->sendMessage($text);
+
+        return response()->json("Query submitted! Someone from the team will call you.");
     }
 }
