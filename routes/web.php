737c46c8b2c9a25b5c27c0ca87c13ff5a1be32bf
Sayan <sayan.das@idevlive.com>
2025-11-04 20:00:52 +0530

    Enhance routing for blog management; fetch latest posts for homepage and define blog routes

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Post;
+
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\ContactUsController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
-    return view('index');
+    $posts = Post::orderBy("id", "desc")->get()->take(3);
+    return view('index', ["posts" => $posts]);
 });
 
 Route::view("about-us", "about");
@@ -26,7 +31,10 @@ Route::view("pricing/quickbooks-setup-and-consultation", "pricing/qbconsultation
 Route::view("pricing/quickbooks-bookkeeping", "pricing/qbbookkeeping");
 
 Route::view("resources", "resources");
-Route::view("resources/blog", "resources/blog");
+
+Route::get("resources/blog", [PostController::class, "index"]);
+Route::get("resources/blog/{url}", [PostController::class, "show"]);
+
 Route::view("resources/faq", "resources/faq");
 
 Route::view("terms-of-service", "terms");
@@ -35,3 +43,12 @@ Route::view("refund-policy", "refund");
 
 Route::view("contact-us", "contact");
 Route::post("contact-us", [ContactUsController::class, "store"]);
+
+
+# Routes for uploading and managing blogs
+Route::view("admin/login", "admin/login")->name("login");
+Route::post("admin/login", [UserController::class, "login"]);
+
+Route::group(["middleware" => "auth", "prefix" => "admin"], function () {
+    Route::get("blogs", [PostController::class, "index"]);
+});
