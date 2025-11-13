be80216f3ae4c93330f6d1fba2d9b21e2279289a
Sayan <sayan.das@idevlive.com>
2025-11-13 10:25:09 +0530

    Update blog post template with dynamic title and enhanced phone number display

--- resources/views/resources/post.blade.php
+++ resources/views/resources/post.blade.php
@@ -55,15 +55,25 @@
 			]
 		]
 	])
-	@slot('title')Our Blog @endslot
+	@slot('title'){{ $post->title }} @endslot
 	@slot('description')@endslot
 @endcomponent
 
 <style>
+	.header-content h1 {
+		font-size: 4.2rem;
+	}
+
 	h1 {
 		font-size: 1.64rem;
 		line-height: 110%;
 		margin: 1.0933333333rem 0 .656rem 0;
+	}
+
+	h1.phone-number {
+		font-size: 4.2rem;
+		color: white;
+		font-family: var(--header-font);
 	}
 
 	h2 {
@@ -77,18 +87,44 @@
 		font-weight: 400;
 		color: #21221;
 	}
+
+	@media screen and (max-width: 1440px) {
+		h1.phone-number {
+			font-size: 3.2rem !important;
+		}
+	}
+
+	@media screen and (min-width: 1440px) and (max-width: 1680px) {
+		h1.phone-number {
+			font-size: 3.8rem;
+		}
+	}
 </style>
 
 <section>
 	<div class="container">
 		<div class="row">
-			<div class="col s12 m6 l5" style="position: sticky; top: 96px">
-				<h5 class="header-font" style="margin-top: 0">Ready to simplify your bookkeeping?</h5>
-				<p class="grey-text text-darken-2">Talk to our accounting experts today to discover flexible bookkeeping solutions tailored to your small business.</p>
+			<div class="col s12 white hide-on-med-and-up" style="position: fixed; bottom: 0; left: 0">
+				<div class="card-panel z-depth-0" style="margin: 0">
+					<a
+						href="tel:{{ env("PHONE") }}"
+						class="btn-large full-width"
+						style="background-color: #2ca01c; color: white !important; font-size: 1.5rem"
+					>
+						Call Us At {{ env("PHONE") }}
+					</a>
+				</div>
 
-				<a href="tel:{{ env("PHONE") }}" class="btn-large full-width yellow darken-2" style="margin-top: 2rem">Call Us At {{ env("PHONE") }}</a>
-
-				<p style="margin-top: 2rem">Or, want to schedule a callback? <a href="#book-consultation" class="modal-trigger yellow-text text-darken-4">Book free consultation</a>.</p>
+				{{-- <p style="margin-top: 2rem">Or, want to schedule a callback? <a href="#book-consultation" class="modal-trigger yellow-text text-darken-4">Book free consultation</a>.</p> --}}
+			</div>
+			<div class="col m6 l5 hide-on-small-only center-align" style="position: sticky; top: 96px">
+				<img src="{{ asset("images/proadvisor.png") }}" alt="quick accounting services" width="25%">
+				<p class="grey-text text-darken-1">{{ env("COMPANY") }} is a certified QuickBooks ProAdvisor and is not affiliated with QuickBooks in any other way. {{ env("COMPANY") }} is a third party company trained to help users with QuickBooks&reg;.</p>
+				<div class="card-panel z-depth-0" style="background-color: #2ca01c">
+					<h5 class="header-font white-text">Need support for bookkeeping?</h5>
+					<h1 class="phone-number">{{ env("PHONE") }}</h1>
+					<p class="white-text">Talk to our accounting experts today to discover flexible bookkeeping solutions tailored to your small business.</p>
+				</div>
 			</div>
 
 			<div class="col s12 m6 l6 offset-l1">
