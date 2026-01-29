9d9da96889d4b92d4f520a3dee4be144b40e5cdd
Sayan <sayan.das@idevlive.com>
2026-01-29 16:52:04 +0530

    Fix syntax for title and meta tags in QuickBooks template

--- resources/views/templates/quickbooks.blade.php
+++ resources/views/templates/quickbooks.blade.php
@@ -4,10 +4,10 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>{{ @yield("title") }} | Quick Accounting Services</title>
+	<title>@yield("title") | Quick Accounting Services</title>
 
-	<meta name="description" content="{{ @yield("description") }}" />
-	<meta name="keywords" content="{{ @yield("keywords")}}" />
+	<meta name="description" content="@yield("description")" />
+	<meta name="keywords" content="@yield("keywords")" />
 
 	<link rel="canonical" href="https://quickaccountingservices.com/qbcf-monitor-service-not-running" />
 
